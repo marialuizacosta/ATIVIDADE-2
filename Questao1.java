709c8e1a7328e08db6ded72d92bcb55b4705d837
marialuizacosta <88732902+marialuizacosta@users.noreply.github.com>
2021-10-14 14:33:00 -0300

    Rename Questao1.java.txt to Questao1.java

--- Questao1.java
+++ Questao1.java
@@ -4,20 +4,20 @@ public static void main(String [] args){
     
 
     
-Paciente paciente1 = new Paciente("00000", "Maria Luíza ","20/12/2004", "Feminino", "Planserv", "Nenhuma", "A+" );
+Paciente paciente1 = new Paciente("00000", "Maria LuÃ­za ","20/12/2004", "Feminino", "Planserv", "Nenhuma", "A+" );
 
 
 
 System.out.println(" \n Dados da Paciente: \n ");
 
-System.out.println("Código: "+paciente1.getCodigo());
+System.out.println("CÃ³digo: "+paciente1.getCodigo());
 
 System.out.println("Nome: "+paciente1.getNome());
 
 System.out.println("Data de Nascimento: "+paciente1.getDataNascimento());
 System.out.println("Sexo: "+paciente1.getSexo());
 
-System.out.println("Plano de Saúde: "+paciente1.getPlanoSaude());
+System.out.println("Plano de SaÃºde: "+paciente1.getPlanoSaude());
 
 System.out.println("Alergia: "+paciente1.getAlergia());
 
@@ -42,9 +42,9 @@ paciente1.setTipoSanguineo("A+");
 
     
 
-System.out.println(" \n Dados da mãe: \n ");
+System.out.println(" \n Dados da mÃ£e: \n ");
 
-System.out.println("Código: "+paciente1.getCodigo());
+System.out.println("CÃ³digo: "+paciente1.getCodigo());
 
 System.out.println("Nome: "+paciente1.getNome());
 
@@ -52,7 +52,7 @@ System.out.println("Data de Nascimento: "+paciente1.getDataNascimento());
 
 System.out.println("Sexo: "+paciente1.getSexo());
 
-System.out.println("Plano de Saúde: "+paciente1.getPlanoSaude());
+System.out.println("Plano de SaÃºde: "+paciente1.getPlanoSaude());
 
 System.out.println("Alergia: "+paciente1.getAlergia());
 
